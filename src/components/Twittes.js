e68c796d2d9848b4e38b5bba1200ef935458a029
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-01-18 22:48:09 +0500

    update all the app and full responsive

--- src/components/Twittes.js
+++ src/components/Twittes.js
@@ -4,12 +4,27 @@ import Posts from "./Posts";
 import Trends from "./Trends";
 
 const Twittes = () => {
+  const trendFun = () => {
+    // var x = document.getElementsByClassName("trends");
+    // var y = x.item("className")
+    // if (y.style.display === "block") {
+    //   y.style.display = "none";
+    //   y.style.width = "300px";
+    // } else {
+    //   y.style.display = "block";
+    // }
+    alert("Wellcome to trend section");
+  }
+
+  const aboutFun = () => {
+    alert("Wellcome to about section");
+  }
   return (
     <div className="posts">
       <div className="posts__home">
         <h1 className="home">Home</h1> 
-        <h2 className="trend">Trends</h2>
-        <h2 className="links">About</h2>
+        <h2 className="trend"  onClick={trendFun}>Trends</h2>
+        <h2 className="links" onClick={aboutFun}>About</h2>
       </div>
       <CreateTweet />
       <Posts />
