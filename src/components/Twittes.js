44991c30d8a702d52a60c4253558490f284a65db
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-01-20 04:16:04 +0500

    set all funtionality of nav bar

--- src/components/Twittes.js
+++ src/components/Twittes.js
@@ -5,29 +5,52 @@ import Trends from "./Trends";
 
 const Twittes = () => {
   var x = document.getElementsByClassName("trends");
+  var a = document.getElementsByClassName("sidebar");
   const trendFun = () => {
     var y = x.item("className")
-    if (y.style.display === "block") {
+    var b = a.item("className")
+    if (y.style.display === "block" || b.style.display === "none") {
       y.style.display = "none";
+      b.style.display = "block"
     } else {
       y.style.display = "block";
+      b.style.display = "none"
     }
   }
 
   const aboutFun = () => {
-    alert("Wellcome to about section");
+    var b = a.item("className")
+    var y = x.item("className")
+    if (b.style.display === "block" || y.style.display === "none") {
+      b.style.display = "none";
+      y.style.display = "block";
+    } else {
+      b.style.display = "block";
+      y.style.display = "none";
+    }
   }
+
+  const homeFun = () => {
+    // alert("wellcome home")
+    var y = x.item("className")
+    var b = a.item("className")
+    y.style.display = "none";
+    b.style.display = "none";
+  }
+ 
   return (
     <div className="posts">
       <div className="posts__home">
-        <h1 className="home">Home</h1> 
+        <h1 className="home" onClick={homeFun}>Home</h1> 
         <h2 className="trend"  onClick={trendFun}>Trends</h2>
         <h2 className="links" onClick={aboutFun}>About</h2>
       </div>
+      <div className="create_tweet_and_post">
       <CreateTweet />
       <Posts />
       <Posts />
       <Posts />
+      </div>
     </div>
   );
 };
